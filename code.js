81259c7a16873a81020ab03e8e2e6b87f3420158
KeyLuce <86408122+Keyluce@users.noreply.github.com>
2023-08-25 13:49:11 +0530

    Add operate function

--- code.js
+++ code.js
@@ -15,3 +15,27 @@ function divide(num1,num2)
     return num1 / num2;
 }
 
+function operate(num1,op,num2)
+{
+    switch (op)
+    {
+        case '+':
+            return add(num1,num2);
+        
+        case '-':
+            return subtract(num1,num2);
+       
+        case '*':
+            return multiply(num1,num2);
+       
+        case '/':
+            return divide(num1,num2);
+         
+    }
+}
+
+let number1;
+let operator;
+let number2;
+
+
